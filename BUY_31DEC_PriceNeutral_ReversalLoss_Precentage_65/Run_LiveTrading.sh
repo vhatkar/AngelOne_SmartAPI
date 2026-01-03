cfed6d5e8c2ef4ce76f66bf64772698f3a92c473
vhatkar <36207104+vhatkar@users.noreply.github.com>
2026-01-03 16:32:37 +0530

    Delete BUY_31DEC_PriceNeutral_ReversalLoss_Precentage_65 directory

--- BUY_31DEC_PriceNeutral_ReversalLoss_Precentage_65/Run_LiveTrading.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Move to the folder where this .sh file is located
-cd "$(dirname "$0")"
-
-# Run the python file in this folder
-sudo /home/pravin/PyCharmMiscProject/.venv/bin/python3 live_trader_main.py
-
